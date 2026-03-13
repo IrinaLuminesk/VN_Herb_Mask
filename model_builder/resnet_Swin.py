4f72fb81f9c327e55aa340b11830997cd1aeeb4a
Irina Luminesk <100247516+IrinaLuminesk@users.noreply.github.com>
2026-03-13 22:01:41 +0700

    Fix Swin

--- model_builder/resnet_Swin.py
+++ model_builder/resnet_Swin.py
@@ -1,8 +1,13 @@
 import torch.nn as nn
-from torchvision.models import resnet50, ResNet50_Weights, resnet18, ResNet18_Weights
+from torchvision.models import resnet50, ResNet50_Weights, resnet18, ResNet18_Weights, swin_v2_b, Swin_V2_B_Weights
 import torch
+import torch.nn.functional as F
 
-import timm
+class CNNtoSwinAdapter(nn.Module):
+    def forward(self, x):
+        x = F.avg_pool2d(x, 2)
+        x = x.permute(0, 2, 3, 1)
+        return x
 
 class Resnet50_Swin(nn.Module):
     def __init__(self, num_classes):
@@ -13,30 +18,27 @@ class Resnet50_Swin(nn.Module):
             resnet_weights = ResNet50_Weights.DEFAULT
             backbone_model = resnet50(weights=resnet_weights)
 
+            swin_weights = Swin_V2_B_Weights.DEFAULT
+            swin_model = swin_v2_b(weights=swin_weights)
+
             self.model_input = nn.Sequential(
                 backbone_model.conv1,
                 backbone_model.bn1,
                 backbone_model.relu,
                 backbone_model.maxpool,
             )
-
+            #Resnet 50
             self.layer1 = backbone_model.layer1
             self.layer2 = backbone_model.layer2
             self.layer3 = backbone_model.layer3
             self.layer4 = backbone_model.layer4
             
-            self.res_proj = nn.Conv2d(2048, 1024, kernel_size=1)
-            self.swin_proj = nn.Identity()
-            self.swin_norm = nn.LayerNorm(1024)
 
-            swin_backbone = timm.create_model(
-                 "swin_base_patch4_window7_224",
-                 pretrained=True
-            )
+            #Swin
+            self.swin_layer = swin_model.features[7]
 
-            self.swin_stage3 = swin_backbone.layers[2]
-            self.swin_stage4 = swin_backbone.layers[3]
-
+            self.adapt_cnn_2_Swin = CNNtoSwinAdapter() #Dùng để đổi [B, 16, 16, 1024] sang [B, 1024, 8, 8]
+            self.swin_proj = nn.Conv2d(1024, 2048, 1)
             self.avgpool = backbone_model.avgpool
             self.fc = nn.Sequential(
                     nn.Linear(2048, 1024),
@@ -53,19 +55,16 @@ class Resnet50_Swin(nn.Module):
         x = self.layer1(x)
         x= self.layer2(x)
         #Layer này chia nhánh ra 2 nhánh, nhánh 1 vào layer4 gốc của Resnet và nhánh 2 vào stage 3 và 4 của Swin
-        shared = self.layer3(x) #(1024, 14, 14)
+        shared = self.layer3(x) #(1024, 16, 16)
 
         #Branch 1
-        resnet_branch = self.layer4(shared) #(2048, 7, 7)
-        resnet_branch = self.res_proj(resnet_branch)
-        #Branch 2
-        swin_branch = shared.permute(0,2,3,1)  # BCHW -> BHWC
-        swin_branch = self.swin_norm(swin_branch)
-        swin_branch = self.swin_stage3(swin_branch)
-        swin_branch = self.swin_stage4(swin_branch)
-        swin_branch = swin_branch.permute(0,3,1,2) #BHWC -> BCHW
-        swin_branch = self.swin_proj(swin_branch)
+        resnet_branch = self.layer4(shared) #(2048, 8, 8)
         
+        #Branch 2
+        swin_branch = self.adapt_cnn_2_Swin(shared)  # BCHW -> BHWC
+        swin_branch = self.swin_layer(swin_branch)
+        swin_branch = swin_branch.permute(0, 3, 1, 2).contiguous()
+        swin_branch = self.swin_proj(swin_branch)
         x = torch.cat([resnet_branch, swin_branch], dim=1) 
 
         x = self.avgpool(x)
