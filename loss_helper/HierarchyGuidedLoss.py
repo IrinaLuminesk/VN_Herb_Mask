Fix dimension error in softmax function and accumulating error in metricCal

loss_helper/HierarchyGuidedLoss.py
@@ -37,8 +37,10 @@ class HierarchyGuidedLoss(nn.Module):
         loss = dict()
         for key in hier_matrixs.keys():
             x_name, y_name = key.split("2")
-            x = torch.softmax(logits[x_name], dim=0)
-            y = torch.softmax(logits[y_name], dim=0)
+            # x = torch.softmax(logits[x_name], dim=0)
+            # y = torch.softmax(logits[y_name], dim=0)
+            x = torch.softmax(logits[x_name], dim=1)
+            y = torch.softmax(logits[y_name], dim=1)
             xy_score = torch.einsum("bi,ij,bj->b", x, hier_matrixs[key], y)
             loss_xy = loss[key] = -torch.log(xy_score + 1e-8).mean()
             consistency_loss += loss_xy

train3.py
@@ -176,7 +176,7 @@ def main():
     if enabled_batchwise_transform:
         batchWiseAug = BatchWiseAug(config=config, num_classes=num_classes)
 
-    model = Model(model_type=model_type, num_classes=num_classes)
+    model = Model(model_type=model_type, num_classes=num_classes).to(device)
     # model = Resnet50_Hierarchy(num_classes, 0, False).to(device)
 
     eval_criterion = nn.CrossEntropyLoss()

utils/MetricCal_Hier.py
@@ -117,7 +117,7 @@ class MetricCal_Hier():
         pred_class = dict()
         true_class = dict()
         for index, key in enumerate(self.num_classes.keys()):
-            self.each_cls_loss[key] = each_cls_loss[key].detach() * batch_size
+            self.each_cls_loss[key] += each_cls_loss[key].detach() * batch_size
             if type == "soft":
                 pred_class[key] = outputs[key].argmax(dim=1)
                 # true_class[key] = targets[:, index].argmax(dim=1)
@@ -141,7 +141,7 @@ class MetricCal_Hier():
             self.fn_per_class[key] += cm.sum(dim=1) - cm.diag()
 
         for key in self.consistent_list:
-            self.each_consistent_loss[key] = each_consistent_loss[key].detach() * batch_size
+            self.each_consistent_loss[key] += each_consistent_loss[key].detach() * batch_size
 
         self.total += batch_size
 